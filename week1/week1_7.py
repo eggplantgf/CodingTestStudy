dfb6899c1f5f61b4c928cb59ba901ae535c07371
MINJUNG KIM <kmj0752@gmail.com>
2024-09-27 19:32:41 +0900

    Day3 commit

---
+++ week1/week1_7.py
@@ -0,0 +1,14 @@
+# 분수의 덧셈
+
+def gcd(a,b) :
+    if b == 0:
+        return a
+    else :
+        return gcd(b,a%b)
+
+def solution(numer1, denom1, numer2, denom2):
+    a = numer1*denom2 + numer2*denom1
+    b = denom1*denom2
+    if gcd(a,b) != 1: # 기약분수 변경
+        a, b = a//gcd(a,b) , b//gcd(a,b)
+    return [a,b]
---
+++ week1/week1_9.py
@@ -0,0 +1,5 @@
+# 나머지 구하기
+
+def solution(num1, num2):
+    answer = int(num1 % num2)
+    return answer
