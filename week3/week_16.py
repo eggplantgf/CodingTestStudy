2afeb3f120265ae06a4dc4ddb00faa0e26ac27f9
MINJUNG KIM <kmj0752@gmail.com>
2024-10-15 23:13:12 +0900

    Day17 commit

---
+++ week3/week_16.py
@@ -0,0 +1,17 @@
+# ox 퀴즈
+
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        arr = i.split()
+        if arr[1] == "+":
+            if int(arr[0]) + int(arr[2]) == int(arr[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+        elif arr[1] == "-":
+            if int(arr[0]) - int(arr[2]) == int(arr[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+    return answer
