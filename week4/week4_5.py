050a6390c4a884827bda31a0c03041f1c8790be0
MINJUNG KIM <kmj0752@gmail.com>
2024-10-28 23:39:39 +0900

    Day20 commit

---
+++ week4/week4_5.py
@@ -0,0 +1,13 @@
+# 직사각형 넓이 구하기
+
+def solution(dots):
+    if dots[0][0] == dots[1][0]:
+        h = dots[2][0] - dots[0][0]
+    else:
+        h = dots[1][0] - dots[0][0]
+    if dots[0][1] == dots[1][1]:
+        w = dots[2][1] - dots[0][1]
+    else:
+        w = dots[1][1] - dots[0][1]
+    return abs(h) * abs(w)
+        
