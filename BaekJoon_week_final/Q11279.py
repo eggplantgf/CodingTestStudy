f649f1326dc46695ae50fa1f5aa051127e86ac71
MINJUNG KIM <kmj0752@gmail.com>
2024-11-28 23:46:47 +0900

    week_final

---
+++ BaekJoon_week_final/Q11279.py
@@ -0,0 +1,19 @@
+# 최대 힙
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+
+for i in range(n):
+    order = int(input())
+
+    if order == 0:
+        if len(arr)==0:
+            print(0)
+        else:
+            print(-1*heapq.heappop(arr))
+    else:
+        heapq.heappush(arr, -1*order)
---
+++ BaekJoon_week_final/Q1927.py
@@ -0,0 +1,19 @@
+# 최소 힙
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+
+for i in range(n):
+    order = int(input())
+
+    if order == 0:
+        if len(arr)==0:
+            print(0)
+        else:
+            print(heapq.heappop(arr))
+    else:
+        heapq.heappush(arr, order)
